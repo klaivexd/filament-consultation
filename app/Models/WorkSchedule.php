4be05ac897d236b587af8b92d2995c23e88f7c0f
Chrisjohn Laxa <chrisjohn.laxa@exocoder.io>
2025-01-09 19:59:00 +0800

    small fixes again

--- app/Models/WorkSchedule.php
+++ app/Models/WorkSchedule.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TimePicker;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -29,6 +32,62 @@ class WorkSchedule extends Model
         'staff_id' => 'integer',
         'work_schedule_consultation_category_id' => 'integer',
     ];
+
+    public function getDayOfWeekNameAttribute(): string
+    {
+        $days = [
+            0 => 'Sunday',
+            1 => 'Monday',
+            2 => 'Tuesday',
+            3 => 'Wednesday',
+            4 => 'Thursday',
+            5 => 'Friday',
+            6 => 'Saturday',
+        ];
+
+        return $days[$this->day_of_week] ?? 'Unknown';
+    }
+
+
+    public static function getForm($staffId = null): array
+    {
+        return [
+            DatePicker::make('date_from')
+                ->label('Valid from (Date)')
+                ->required(),
+            DatePicker::make('date_until')
+                ->label('Valid until (Date)')
+                ->required(),
+            Select::make('day_of_week')
+                ->label('Day of Week')
+                ->options([
+                    0 => 'Sunday',
+                    1 => 'Monday',
+                    2 => 'Tuesday',
+                    3 => 'Wednesday',
+                    4 => 'Thursday',
+                    5 => 'Friday',
+                    6 => 'Saturday',
+                ])
+                ->required(),
+            TimePicker::make('time_from')
+                ->label('Valid from (Time)')
+                ->required(),
+            TimePicker::make('time_until')
+                ->label('Valid until (Time)')
+                ->required(),
+            Select::make('staff_id')
+                ->hidden(function () use ($staffId) {
+                    return $staffId !== null;
+                })
+                ->relationship('staff', 'first_name')
+                ->required(),
+            Select::make('work_schedule_consultation_category_id')
+                ->label('Consultation Category')
+                ->relationship('workScheduleConsultationCategories.consultationCategory', 'title')
+                ->required(),
+        ];
+    }
 
     public function staff(): BelongsTo
     {
